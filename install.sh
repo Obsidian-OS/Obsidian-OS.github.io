51081324e494bc67db2aa3160f71b4d524b542be
The Odd Cell <132018273+TheOddCell@users.noreply.github.com>
2025-08-13 15:53:09 -0400

    Create install.sh (#2)

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# install.sh - install ObsidianOS from an normal arch host
+set -e
+
+# Function to check if a command exists
+check_command() {
+  local cmd="$1"
+  local extra_msg="$2"
+  if ! command -v "$cmd" &>/dev/null; then
+    echo "Error: Required command '$cmd' not found. Please install it."
+    [[ -n "$extra_msg" ]] && echo "$extra_msg"
+    exit 1
+  fi
+}
+
+# Check for root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+# Check for needed commands
+check_command git
+check_command make
+check_command python3
+check_command mktemp
+
+# Make temporary directory
+TMPDIR="$(mktemp -d)"
+
+# Clone and prepare obsidian-wizard and obsidianctl
+git clone https://github.com/Obsidian-OS/obsidian-wizard "$TMPDIR/obsidian-wizard"
+git clone https://github.com/Obsidian-OS/obsidianctl "$TMPDIR/obsidianctl"
+bash -c "cd /$TMPDIR/obsidianctl;make"
+chmod u+x "$TMPDIR/obsidian-wizard/obsidian-wizard.py"
+
+exec "$TMPDIR/obsidian-wizard/obsidian-wizard.py"
