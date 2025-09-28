074568889103ed705d545d512bac56a0ecf32878
/home/odd <132018273+TheOddCell@users.noreply.github.com>
2025-09-28 09:10:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-# ba.sh/ARbs - install ObsidianOS from an normal arch host
-# This script has moved...
+:<<:
+ba.sh/ARbs - install ObsidianOS from an normal arch host
+This script has moved however will still function as a redirect.
+HTML redirect 1: <script>window.location.replace("https://arbs.obsidianos.xyz");</script>
+HTML redirect 2: <meta http-equiv="refresh" content="0; url=https://arbs.obsidianos.xyz">
+:
 exec bash -c "$(curl -fsSL https://arbs.obsidianos.xyz)"
