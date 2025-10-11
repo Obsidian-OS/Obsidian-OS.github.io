4c0b7e4645e3e766fde34ec1e8c924be14fe844a
Xiufeng Guo <i@m.ac>
2025-10-12 03:01:03 +0900

    Change short URL to ba.sh/obsidianos

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 :<<:
-ba.sh/ARbs - install ObsidianOS from an normal arch host
+ba.sh/obsidianos - install ObsidianOS from an normal arch host
 This script has moved however will still function as a redirect.
 HTML redirect 1: <script>window.location.replace("https://arbs.obsidianos.xyz");</script>
 HTML redirect 2: <meta http-equiv="refresh" content="0; url=https://arbs.obsidianos.xyz">
