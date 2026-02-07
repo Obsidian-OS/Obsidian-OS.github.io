c3eaa3a12b0dae7cf682039e453d759b60226b25
/home/odd <odd@obsidianos.xyz>
2026-02-07 15:35:33 -0500

    Update and rename style.sh to configure.sh

---
+++ configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$(id -u)" -eq 0 ]; then
+  echo "You are root, please run this script as a non-root user."
+  exit 1
+fi
+echo "Branding ObsidianOS..."
+sudo curl -fL https://obsidianos.xyz/os-release -o /etc/os-release
+echo "Branding /etc/issue..."
+sudo curl -fL https://obsidianos.xyz/issue -o /etc/issue
+echo "Attempting to add ObsidianOS branding packages..."
+if command -v yay >/dev/null 2>&1; then
+  echo "Found yay"
+  yay -S obsidianos-branding obos-genissue-git --overwrite '*' --noconfirm --needed
+fi
+echo "Attempting to add obsidianctl..."
+if command -v yay >/dev/null 2>&1; then
+  echo "Found yay"
+  yay -S obsidianctl-git --overwrite '*' --noconfirm --needed
+fi
+echo "Done!"
+echo "If using Arch based ObsidianOS install the AUR package obos-genissue-git."
